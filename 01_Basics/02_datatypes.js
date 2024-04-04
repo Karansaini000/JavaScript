270dfcc6885b6e8ad2ef21abc53b0588fc3caf07
Karansaini000 <122517764+Karansaini000@users.noreply.github.com>
2024-04-04 04:36:08 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert(3+3) We are Using node JS not browser
+
+let name = "Karan"
+let age = 19
+let loggedIn = true
+
+//  number 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof"Hitesh") // String
+console.log(typeof null) // Object
+console.log(typeof undefined) // Undefined
