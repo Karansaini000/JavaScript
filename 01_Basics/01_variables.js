3d2ec1064a5288e4eeb6fc836516e9136f4dcad0
Karansaini000 <122517764+Karansaini000@users.noreply.github.com>
2024-04-04 04:37:25 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = "12222";
+let accountEmail = "ks5075277@gmail.com";
+var accountPassword = "2211222";
+accountCity = "Jaipur";
+
+// accountId = 23242;
+console.log(accountId);
+
+/* 
+    Prefer not to use var
+    beacuse of issue in block scope and functional Scope
+*/
+
+accountEmail = "hchc@gmail.com";
+accountPassword = "22432412";
+accountCity = "Ludhiana";
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
